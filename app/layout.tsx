4168ff03fc5ad9fbb87133cd787da997bcfcdada
Chutiparts <tookjai5988@gmail.com>
2026-06-12 13:43:52 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Header from '@/components/layout/Header'
 import { CartProvider } from './context/CartContext'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import ContactHub from '@/components/ContactHub'
 
 const SITE_URL = 'https://chutibenz.com'
 const BRAND = 'ChutiBenz'
@@ -146,6 +147,7 @@ export default function RootLayout({
 
           <Analytics />
           <SpeedInsights />
+          <ContactHub />
         </CartProvider>
       </body>
     </html>
